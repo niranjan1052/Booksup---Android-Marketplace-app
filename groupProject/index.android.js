cb3dbf2d6bc28e7683a07ae4e333cce99163e87c
Patrick Richard <rich1044@umn.edu>
2016-11-29 18:41:27 -0600

    added app drawer

---
+++ groupProject/index.android.js
@@ -0,0 +1,6 @@
+import React, { Component } from 'react';
+import { AppRegistry, Button, DrawerLayoutAndroid, Text, TextInput, View } from 'react-native';
+
+import TheComponent from './client/drawer.js'
+
+AppRegistry.registerComponent('groupProject', () => TheComponent);
