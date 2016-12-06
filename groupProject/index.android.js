d2812114d98a211cb9e039b9f3c8fb4489e1129f
asundark <ajaysundar.k@gmail.com>
2016-12-05 20:04:47 -0600

    deleted groupProject directory

--- groupProject/index.android.js
+++
@@ -1,6 +0,0 @@
-import React, { Component } from 'react';
-import { AppRegistry, Button, DrawerLayoutAndroid, Text, TextInput, View } from 'react-native';
-
-import TheComponent from './client/drawer.js'
-
-AppRegistry.registerComponent('groupProject', () => TheComponent);
